9133f498ad3275a2a624534572af2cdcf201e0b7
Juan Henao <parjuag@gmail.com>
2020-08-03 08:17:28 -0500

    Organize the saving process and execute it

--- src/handler/sync_all.go
+++ src/handler/sync_all.go
@@ -6,12 +6,24 @@ import (
 	"net/http"
 	"strconv"
 	"time"
+
+	"../db"
+	"../models/assignment"
+	"../models/device"
+	"../models/ip"
+	"../models/transaction"
 )
 
 // SyncAll the repos with one date
 func SyncAll(w http.ResponseWriter, r *http.Request) {
+
+	// Create Main Variables
 	var nDate int
 	var err error
+	var assignments assignment.AssignmentsRepo
+	var transactions transaction.Repo
+	var ips ip.Repo
+	var devices device.Repo
 
 	// Get date param
 	date := r.URL.Query().Get("date")
@@ -26,14 +38,24 @@ func SyncAll(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	fmt.Printf("Fecha a sincronizar: %v\n", date)
+	fmt.Printf("Fecha a sincronizar: %v\n", nDate)
 
-	buyers := syncBuyers(nDate)
-	products := syncProducts(nDate)
-	transactions := syncTransactions(&nDate)
+	// Create connection with Dgraph
+	dg := db.NewClient()
+
+	_, buyersAssignments := syncBuyers(nDate, dg)
+	_, productsAssignments := syncProducts(nDate, dg)
+
+	assignments.Buyers = buyersAssignments
+	assignments.Products = productsAssignments
+
+	// Once everything are saved pass params and save in transactions
+	assignments.Transactions = syncTransactions(&nDate, &transactions, &ips, &devices, &assignments, dg)
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(buyers.Buyers[:10])
-	json.NewEncoder(w).Encode(products.Products[:10])
-	json.NewEncoder(w).Encode(transactions.Transactions[:10])
+	// json.NewEncoder(w).Encode(buyers.Buyers[:10])
+	// json.NewEncoder(w).Encode(products.Products[:10])
+	// fmt.Printf("Transactions: %v, IPS: %v\n", len(transactions.GetAll()), len(ips.GetAll()))
+	json.NewEncoder(w).Encode(transactions.Transactions[:100])
+	// json.NewEncoder(w).Encode(ips.IPS[:10])
 }
