202fb49f99bc09b1036b4896c816d1117a1a9896
Juan Henao <parjuag@gmail.com>
2020-08-03 08:16:31 -0500

    Parse, save and get uids of devices

---
+++ src/handler/sync_devices.go
@@ -0,0 +1,57 @@
+package handler
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/dgraph-io/dgo"
+	"github.com/dgraph-io/dgo/protos/api"
+
+	"../models/device"
+)
+
+//SyncDevices return a list of handled ips
+func SyncDevices(listDevices []string, devices *device.Repo, dg *dgo.Dgraph) map[string]string {
+	fmt.Printf("Devices: Len>: %v\n", len(listDevices))
+
+	for _, v := range listDevices {
+		devices.Add(device.Device{
+			UID:  "_:" + v,
+			Name: v,
+		})
+	}
+
+	devicesAssignments := saveDevices(devices, dg)
+
+	return devicesAssignments
+}
+
+func saveDevices(devices *device.Repo, dg *dgo.Dgraph) map[string]string {
+	ctx := context.Background()
+	mu := &api.Mutation{
+		CommitNow: true,
+	}
+
+	fmt.Printf("Length of devices: %v\n", len(devices.Devices))
+
+	pb, err := json.Marshal(devices.Devices)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mu.SetJson = pb
+
+	assignments, err := dg.NewTxn().Mutate(ctx, mu)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := range devices.Devices {
+		devices.Devices[i].UID = assignments.Uids[devices.Devices[i].Name]
+	}
+
+	return assignments.Uids
+}
