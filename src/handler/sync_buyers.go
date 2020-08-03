80f5b76d86280f579f9baaef86f910b12fd1af5d
Juan Henao <parjuag@gmail.com>
2020-08-03 08:16:12 -0500

    Parse, save and get uids in database for buyers type

--- src/handler/sync_buyers.go
+++ src/handler/sync_buyers.go
@@ -1,15 +1,21 @@
 package handler
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
+
+	// Using DGraph
+	"github.com/dgraph-io/dgo"
+	"github.com/dgraph-io/dgo/protos/api"
 
 	"../models/buyer"
 	"../utils"
 )
 
 // Get all buyers from endpoint
-func syncBuyers(date int) buyer.Repo {
+func syncBuyers(date int, dg *dgo.Dgraph) (buyer.Repo, map[string]string) {
 
 	// Read the body and manage errors
 	body, err := utils.ExtractDataFrom("buyers", date)
@@ -19,13 +25,45 @@ func syncBuyers(date int) buyer.Repo {
 
 	// Create a var buyers of type Repo and conversionError
 	var buyers buyer.Repo
-	myDtype := []string{"Buyer"}
 
 	json.Unmarshal(body, &buyers.Buyers) // Parse the bytes readed from the body in a Repo Property
 
+	buyers, assignments := saveBuyers(buyers, dg)
+
+	return buyers, assignments
+}
+
+func saveBuyers(buyers buyer.Repo, dg *dgo.Dgraph) (buyer.Repo, map[string]string) {
+	ctx := context.Background()
+	mu := &api.Mutation{
+		CommitNow: true,
+	}
+
+	myDtype := []string{"Buyer"}
 	for i := range buyers.Buyers {
+		buyers.Buyers[i].UID = "_:" + buyers.Buyers[i].ID
 		buyers.Buyers[i].DType = myDtype
 	}
 
-	return buyers
+	fmt.Printf("Length of buyers: %v\n", len(buyers.Buyers))
+
+	pb, err := json.Marshal(buyers.Buyers)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mu.SetJson = pb
+
+	assignments, err := dg.NewTxn().Mutate(ctx, mu)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := range buyers.Buyers {
+		buyers.Buyers[i].UID = assignments.Uids[buyers.Buyers[i].ID]
+	}
+
+	return buyers, assignments.Uids
+
 }
