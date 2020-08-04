0938446ec7a46139db44a3e3dc507370582260f1
Juan Henao <parjuag@gmail.com>
2020-08-03 23:18:17 -0500

    Add dynamic pagination to buyers view

--- front-store/src/views/Buyers.vue
+++ front-store/src/views/Buyers.vue
@@ -1,12 +1,32 @@
 <template>
 <div class="container mt-4 text-center">
-  <div class="row">
+  <div class="row my-5">
     <div class="col-md-12">
       <h1>View All Buyers</h1>
     </div>
   </div>
+  <div class="row my-3">
+    <b-paginate>
+      <div id="table-page">
+            <b-pagination
+                v-model="currentPage"
+                :total-rows="rows+1"
+                :per-page="perPage"
+                @input="prepareNextPage()"
+                aria-controls="users-table"
+            ></b-pagination>
+        </div>
+    </b-paginate>
+  </div>
   <div class="row">
-    <b-table id="buyers-table" striped hover :fields="fields" :items="allBuyers">
+    <b-table
+      id="buyers-table"
+      striped hover
+      :fields="fields"
+      :items="allBuyers"
+      :perPage="perPage"
+      :currentPage="currentPage"
+    >
       <template v-slot:cell(actions)="data">
         <b-button
               :to="{name: 'Profile', params: {id: data.item.id}}"
@@ -28,7 +48,8 @@ export default {
   name: 'BuyersView',
   data() {
     return {
-      first: 50,
+      perPage: 50,
+      currentPage: 1,
       fields: [
         { key: 'id', label: 'ID' },
         { key: 'name', label: 'Name' },
@@ -38,13 +59,23 @@ export default {
     }
   },
   computed: {
-    ...mapState(['allBuyers'])
+    ...mapState(['allBuyers']),
+    rows: function(){
+      return this.allBuyers.length;
+    },
   },
   methods: {
     ...mapActions(['getAllBuyers']),
+    prepareNextPage(){
+      if(this.allBuyers.length < this.perPage*this.currentPage){
+        this.getAllBuyers([this.perPage, this.allBuyers.length]);
+      }
+    },
   },
   created(){
-    this.getAllBuyers([this.first, 0])
+    if(this.allBuyers.length == 0){
+      this.getAllBuyers([this.perPage, 0])
+    }
   }
 }
 </script>
